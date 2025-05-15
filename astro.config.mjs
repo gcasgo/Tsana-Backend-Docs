295e6d9c9116176cf2e58097ee669ac4e5708cd0
Guido Castillo <gcasgo@gmail.com>
2025-05-15 14:58:03 -0500

    feat: update site and base configuration in Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://gcasgo.github.io',
+  	base: 'Tsana-Backend-Docs',
 	integrations: [
 		starlight({
 			title: 'Tsana Backend',
